9cf578932e7027540c246b7c7aae44d1c534b95d
imgesalci <91889435+imgesalci@users.noreply.github.com>
2023-07-27 15:14:01 +0300

    added-css

--- inserting.php
+++ inserting.php
@@ -43,25 +43,105 @@ if (isset($_POST['submit']))
 <html>
     <head>
         <title>products</title>  
+        <style>
+        .edit {
+          background-color: #303841;
+          color: #eeeeee;
+          padding: 8px;
+          text-align: center;
+          text-decoration: none;
+          display: inline-block;
+          font-size: 10px;
+          margin: 4px 2px;
+          cursor: pointer;
+          border: 2px solid #303841;
+        }
+
+        .edit:hover {
+          background-color: #eeeeee;
+          color: #7aa5d2;
+          border: 2px solid #7aa5d2;
+        }
+
+        .btn {
+          background-color: #303841;
+          color: #eeeeee;
+          padding: 8px;
+          text-align: center;
+          text-decoration: none;
+          display: inline-block;
+          font-size: 10px;
+          margin: 8px 3px;
+          cursor: pointer;
+          border: 2px solid #303841;
+        }
+
+        .btn:hover {
+          background-color: #eeeeee;
+          color: #7aa5d2;
+          border: 2px solid #7aa5d2;
+        }
+
+        .edit {border-radius: 4px;}
+        .btn {border-radius: 4px;}
+
+        .sbmt {
+          background-color: #303841;
+          color: #eeeeee;
+          padding: 10px;
+          text-align: center;
+          text-decoration: none;
+          display: inline-block;
+          font-size: 10px;
+          margin: 3px 2px;
+          cursor: pointer;
+          border: 2px solid #303841;
+        }
+
+        .sbmt:hover {
+          background-color: #eeeeee;
+          color: #7aa5d2;
+          border: 2px solid #7aa5d2;
+        }
+
+        .sbmt {border-radius: 4px;}
+
+        .box{
+          text-align: center; 
+        }
+
+        .input{
+          background-color: #f1f6fa;
+          border: 2px solid #47555e;
+          border-radius: 4px;
+        }
+
+        body {
+          background-color: #eeeeee;         
+          font-family: 'Lexend Deca', sans-serif;
+          margin: auto;
+          color: #47555e;
+        }
+        </style>
+        
     </head>
     <body>
       <form action="" method = "POST">
-        <div style="width:460px;height:460px;border:.5px solid #000;">
-          &nbsp;Ürün Adı<br>&nbsp;
-          <input name="urun_adi" class="form-control" size="30" style="height:30px"  type="text" required>
+        <div class="box"; style="width:360px;height:500px;border:.5px;">
+          &nbsp;<br>Ürün Adı*<br>&nbsp;
+          <input name="urun_adi" class="input" size="30" style="height:30px"  type="text" required>
           <br><br>&nbsp;Alış Fiyatı<br>&nbsp;
-          <input name="alis_fiyati" size="30" style="height:30px"  type="float">
+          <input name="alis_fiyati" class="input" size="30" style="height:30px"  type="float">
           <br><br>&nbsp;Satış Fiyatı<br>&nbsp;
-          <input name="satis_fiyati" size="30" style="height:30px"  type="float">
+          <input name="satis_fiyati" class="input" size="30" style="height:30px"  type="float">
           <br><br>&nbsp;KDV Oranı<br>&nbsp;
-          <input name="kdv" size="30" style="height:30px"  type="float">
+          <input name="kdv" class="input" size="30" style="height:30px"  type="float">
           <br><br>&nbsp;Ürün Resmi<br>&nbsp;
-          <input name="urun_resmi" size="30" style="height:30px"  type="text">
+          <input name="urun_resmi" class="input" size="30" style="height:30px"  type="text">
           <br><br>&nbsp;Stok Durumu<br>&nbsp;
-          <input name="stok" size="30" style="height:30px"  type="int">
-          <br><br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-          <button type ="submit" style="height:20px" name="submit" value="send to database"> Ekle </button>
-          <input type="reset" style="height:20px">
+          <input name="stok" class="input" size="30" style="height:30px"  type="int">
+          <br><br>&nbsp;&nbsp;&nbsp;
+          <button type ="submit" name="submit" class="sbmt"> Ekle </button>
           <br><br>
         </div><br>
       </form>
--- listing.php
+++ listing.php
@@ -4,12 +4,37 @@ include('inserting.php');
     $result = $conn->query($query);
     
 ?>  
+    <head>
+    <style>::after
+        table {
+            border-collapse: collapse;
+        }
+        table th {
+            background-color: #303841;
+            color: #f7f7f7;
+            border-radius: 2px;
+        }
+        
+        table td{
+            background-color: #eeeeee;
+            border: 1px solid #47555e;
+            border-radius: 2px;
+            padding: 10px 20px;
+        }
+        
+        table tr:hover td {
+            background-color: #d7e4f1;
+        }
+
+    </style>      
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.0/jquery.min.js"></script>
     <script src="/project/delete.js"></script>
     <script src="/project/update.js"></script>
-    <table border="1" style="width:100%" cellspacing="0" cellpadding="10">
+    </head>
+
+    <table style="width:100%" cellspacing="0" cellpadding="10">
     <tr>
-        <th>ID</th>
+        <th>#</th>
         <th>Ürün Adı</th>
         <th>Alış Fiyatı</th>
         <th>Satış Fiyatı</th>
@@ -31,9 +56,8 @@ include('inserting.php');
                 <td class="editable" data-column="kdv_oranı"><?php echo $data['kdv_oranı']; ?></td>
                 <td class="editable" data-column="ürün_resmi"><?php echo $data['ürün_resmi']; ?></td>
                 <td class="editable" data-column="stok_durumu"><?php echo $data['stok_durumu']; ?></td>
-                <td><button class="edit" data-id="<?php echo $data['id']; ?>">Güncelle</button></td>
-                <td>
-                <td>
+                <td align="center"><button class="edit" data-id="<?php echo $data['id']; ?>">Güncelle</button></td>
+                <td align="center">
                     <form action="" method="post">
                         <button class='btn' type="button" name='delete' data-id="<?php echo $data['id']; ?>">Sil</button>
                     </form>
